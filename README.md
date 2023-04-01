4454b85946092fc6dc3c005513ddf9ab29f344db
Isaac Jadrey <jadreyisaac@gmail.com>
2023-04-01 09:57:00 +0300

    update home screen and readme, added src files

---
+++ README.md
@@ -0,0 +1,4 @@
+<div align="center">
+  <h1>OnBoarding UI</h1>
+  <h3>Simple OnBoarding Screen design using jetpack compose</h3>
+</div>
--- app/src/main/java/com/cwj/onboarding/presentation/screens/HomeScreen.kt
+++ app/src/main/java/com/cwj/onboarding/presentation/screens/HomeScreen.kt
@@ -1,18 +1,21 @@
 package com.cwj.onboarding.presentation.screens
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.cwj.onboarding.R
 
+@Preview
 @Composable
 fun HomeScreen() {
     Column(
@@ -20,6 +23,11 @@ fun HomeScreen() {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
+        Image(
+            painter = painterResource(id = R.drawable.undraw_home_screen),
+            contentDescription = "home",
+            modifier = Modifier.size(300.dp).padding(bottom = 16.dp)
+        )
         Text(
             text = stringResource(R.string.welcome),
             style = MaterialTheme.typography.headlineLarge.copy(
